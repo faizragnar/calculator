8a6101225385a0d6a6918d4e7dc9ed4209c8ce60
unknown <faiyazamail@gmail.com>
2025-01-17 15:41:02 +0530

    added base code

--- calculator.java
+++ calculator.java
@@ -12,14 +12,21 @@ class Demo {
     System.out.println("Choose an operator: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    
+    // ask users to enter numbers
     System.out.println("Enter first number");
     number1 = input.nextDouble();
 
     System.out.println("Enter second number");
     number2 = input.nextDouble();
 
+    switch (operator) {
+
+    
    
+      default:
+        System.out.println("Invalid operator!");
+        break;
+    }
 
     input.close();
   }
