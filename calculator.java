af96a59bc14423de0586329524fae1749d84ff2f
unknown <faiyazamail@gmail.com>
2025-01-17 15:29:06 +0530

    added substraction

--- calculator.java
+++ calculator.java
@@ -21,16 +21,18 @@ class Demo {
 
     switch (operator) {
 
-      
-     
-      case '-':
-        result = number1 - number2;
-        System.out.println(number1 + " - " + number2 + " = " + result);
+    
+      // performs multiplication between numbers
+      case '*':
+        result = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + result);
         break;
 
- 
+    
 
-      
+      default:
+        System.out.println("Invalid operator!");
+        break;
     }
 
     input.close();
