7f038d07d26d2817cfe3576ca4817bb54f3f7f0d
unknown <faiyazamail@gmail.com>
2025-01-17 15:24:24 +0530

    added base code

--- calculator.java
+++ calculator.java
@@ -23,11 +23,7 @@ class Demo {
 
       
      
-      case '-':
-        result = number1 - number2;
-        System.out.println(number1 + " - " + number2 + " = " + result);
-        break;
-
+   
  
 
       
